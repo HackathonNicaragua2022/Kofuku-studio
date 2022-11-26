a606d6cf32637e5fc32fe34d3f3a6def56a062f5
Joshua Aburto <obedjoshuaburtolopez@gmail.com>
2022-11-26 07:55:50 -0600

    Se añadio la vista de Contactos

--- Descubre Nica/View/Contacto.xaml.cs
+++ Descubre Nica/View/Contacto.xaml.cs
@@ -1,19 +1,18 @@
-﻿using Descubre_Nica.ViewModel;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Descubre_Nica.ViewModel;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Descubre_Nica.View
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class SettingsPage : ContentPage
+    public partial class Contacto : ContentPage
     {
-        public SettingsPage()
+        public Contacto()
         {
             InitializeComponent();
             BindingContext = new VMContacto(Navigation);
---
+++ Descubre Nica/ViewModel/VMContacto.cs
@@ -0,0 +1,49 @@
+﻿using Descubre_Nica.View;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Descubre_Nica.ViewModel
+{
+    internal class VMContacto:BaseViewModel
+    {
+        #region Variables
+        public string _Texto;
+        #endregion
+        #region Constructores
+        public VMContacto(INavigation navigation)
+        {
+            Navigation = navigation;
+        }
+        #endregion
+        #region Objetos
+        public string Texto
+        {
+            get { return _Texto; }
+            set { SetValue(ref _Texto, value); }
+        }
+        #endregion
+        #region Procesos
+        public async Task NavContacto()
+        {
+            await Navigation.PushAsync(new Contacto());
+        }
+        public async Task Backpage()
+        {
+            await Navigation.PopAsync();
+        }
+        public void ProcesoSimple()
+        {
+
+        }
+        #endregion
+        #region Comandos
+        public ICommand NaveContactoCommand => new Command(async () => await NavContacto());
+        public ICommand BackPageCommand => new Command(async () => await Backpage());
+        public ICommand ProcesoSimpCommand => new Command(ProcesoSimple);
+        #endregion
+    }
+}
